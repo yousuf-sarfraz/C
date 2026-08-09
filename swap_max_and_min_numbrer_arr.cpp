5ee96e565c2b3e8ece1a6bead0e1108ba8e193f6
yousuf-sarfraz <yousufsarfraz27@gmail.com>
2026-08-09 22:17:29 +0500

    Fix min and max index initialization

--- swap_max_and_min_numbrer_arr.cpp
+++ swap_max_and_min_numbrer_arr.cpp
@@ -1,42 +1,57 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void swapminmax(int arr[],int n){
-        int max=arr[0];
-    for(int i=0; i<n; i++)
+void swapminmax(int arr[], int n)
+{
+    int max = 0;
+
+    for (int i = 0; i < n; i++)
     {
-        if(arr[max]<arr[i]){
-            max=i;
+        if (arr[max] < arr[i])
+        {
+            max = i;
         }
     }
-        int min=arr[0];
-    for(int i=0; i<n; i++)
-    {
-        if(arr[min]>arr[i]){
-            min=i;
-        }
-    }    
-    int temp=arr[min];
-     arr[min]=arr[max];
-     arr[max]=temp;
 
+    int min = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[min] > arr[i])
+        {
+            min = i;
+        }
+    }
+
+    int temp = arr[min];
+    arr[min] = arr[max];
+    arr[max] = temp;
 }
-int main(){
-    int n=5;
-    int arr[5]={2,5,7,9,4};
-    cout<<"arr=";
-     for(int i=0; i<n; i++){
-        cout<<arr[i];
-    }
-    
-    cout<<endl;
-    cout<<"swap arr=";
-    swapminmax(arr,n);
-    for(int i=0; i<n; i++){
-        cout<<arr[i];
+
+int main()
+{
+    int n = 5;
+    int arr[5] = {2, 5, 7, 9, 4};
+
+    cout << "arr = ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
 
-    cout<<endl;
+    cout << endl;
+
+    cout << "swap arr = ";
+
+    swapminmax(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
 
     return 0;
 }
