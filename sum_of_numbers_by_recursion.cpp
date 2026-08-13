48b8a3704d050d8d784583bb37ef651070d76d10
yousuf-sarfraz <yousufsarfraz27@gmail.com>
2026-08-13 22:11:33 +0500

    Add recursive sum of numbers

---
+++ sum_of_numbers_by_recursion.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int sum(int n){
+
+    if(n==1){
+        return 1;
+    }
+
+    return n + sum(n-1);
+}
+int main(){
+    
+    cout<<sum(12)<<endl;
+
+    return 0;
+
+}
